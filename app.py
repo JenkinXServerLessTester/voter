eee8030d3211a8282793d61255d81845b8de9c39
q13117god <carlxie2015@gmail.com>
2019-07-23 01:31:02 +0000

    test again

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import random
 import json
 
 option_a = os.getenv('OPTION_A', "A little Calm Cloud Cats")
-option_b = os.getenv('OPTION_B', "A little Calm Cloud Dogs")
+option_b = os.getenv('OPTION_B', "Cloud Dogs")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
